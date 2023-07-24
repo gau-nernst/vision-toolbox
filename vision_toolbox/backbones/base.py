d7755d712bf44039b2cf891a6a08d900b2c5ec24
Thien Tran <gau.nernst@yahoo.com.sg>
2023-07-24 20:57:11 +0800

    remove unnecessary

--- vision_toolbox/backbones/base.py
+++ vision_toolbox/backbones/base.py
@@ -16,11 +16,6 @@ class BaseBackbone(nn.Module, metaclass=ABCMeta):
     def forward(self, x: Tensor) -> Tensor:
         return self.get_feature_maps(x)[-1]
 
-    @staticmethod
-    @abstractmethod
-    def from_config(variant: str, pretrained: bool = False, **kwargs: Any) -> BaseBackbone:
-        pass
-
     def _load_state_dict_from_url(self, url: str) -> None:
         state_dict = torch.hub.load_state_dict_from_url(url)
         self.load_state_dict(state_dict)
